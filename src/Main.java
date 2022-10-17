8b99f9d55c3e2a4f5a833d6e1c70e84781927d99
Felix Chan <felixc63@nycstudents.net>
2022-10-17 12:17:17 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++){
+            int random = (int) (Math.random()*1000) + 1;
+            System.out.println(Math.log(random));
+        }
+    }
 }
--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class RightTriangle {
+public class RightTriangle {
+    private double base;
+    private double height;
+
+    public RightTriangle(double base, double height){
+        this.base = base;
+        this.height = height;
+    }
+
+    public double hypotenuse(){
+        return Math.sqrt((Math.pow(base,2)+Math.pow(height,2)));
+    }
 }
